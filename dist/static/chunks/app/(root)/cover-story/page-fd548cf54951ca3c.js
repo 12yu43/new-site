f9a9c360803effce5dd965e4ad3e69060e7d872a
Ayush Jaiswal <akjaiswal2003@gmail.com>
2024-11-18 16:50:37 +0530

    Delete dist directory

--- dist/static/chunks/app/(root)/cover-story/page-fd548cf54951ca3c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3666],{7042:function(e,n,s){Promise.resolve().then(s.t.bind(s,5878,23)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[4609,2971,2117,1744],function(){return e(e.s=7042)}),_N_E=e.O()}]);
